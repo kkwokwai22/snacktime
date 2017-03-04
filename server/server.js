809ecaa6a4d507cb52c1d2c1525a7dacc47d4dc7
Kevin Wong <kkwokwai22@gmail.com>
2017-03-03 16:26:20 -0800

    app.get and post

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const app = require('./server-config.js')
+const mongoose = require('mongoose');
+
 
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, (err) => {
   console.log('listening on 8000');
 });
+
 
 module.exports = app;
